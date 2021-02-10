281803ec338d77d23292194b14adbe22b8cd5f43
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2021-02-10 12:06:47 +0000

    adding coordinates for standard connectome and updating get_coordinates script

--- get_coordinates.py
+++ get_coordinates.py
@@ -3,11 +3,13 @@ from nilearn.plotting import find_parcellation_cut_coords
 import numpy as np
 
 # SET PATH TO PARCELLATIO NIFTI IMAGE 
-input_path='<insert here>'
+root_path='/home/fs0/exet5033/Connectomes/standardconnectome/parcellation/parcellation-files/'
 
-# SET OUPTUT PATH 
-output_path='<insert here>'
+for i in range(1,6):
+    parcellation_path = root_path + 'sub-01_label-L2018_desc-scale%d_atlas.nii.gz' %i
 
-coordinates = find_parcellation_cut_coords(parcellation)
+    # gets x, y, z coordinates from input nifti image
+    coordinates = find_parcellation_cut_coords(parcellation_path)
 
-np.savetxt(output_path, coordinates)
+    output_path = root_path + 'sub-01_label-L2018_desc-scale%d_atlas_coordinates.csv' %i
+    np.savetxt(output_path, coordinates)
