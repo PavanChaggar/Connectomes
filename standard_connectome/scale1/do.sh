d26c169696171dca5c8166c26caebfe9e1ad8940
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2021-03-08 11:26:45 +0000

    updating do.sh

--- standard_connectome/scale1/do.sh
+++ standard_connectome/scale1/do.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 hcp_folder=/vols/Scratch/HCP
-out_folder=/home/fs0/exet5033/Connectomes/standard_connectome/scale5
+out_folder=/home/fs0/exet5033/Connectomes/standard_connectome/scale1
 filename=/vols/Scratch/HCP/Diffusion/Q1200/all_subjects
 
-rm -rf $out_folder/commands
-mkdir $out_folder/commands
+rm $out_folder/commands/command.txt
+#mkdir $out_folder/commands
 
 
-for ((i = 1 ; i < 51 ; i++)); do
+for ((i = 51 ; i < 101 ; i++)); do
 	subj=$(sed -n $i'p' $filename)
 
     #100307  119025  137431  155635  175136  195445  214019  341834  486759  599671  744553  878877
